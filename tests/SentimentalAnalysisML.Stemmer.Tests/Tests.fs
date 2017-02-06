8b5e7b45ac463c018857eb790384e1e8c3ff90f1
Dominik Kotecki <dominikus1910@gmail.com>
2017-02-06 20:34:26 +0100

    init

---
+++ tests/SentimentalAnalysisML.Stemmer.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module SentimentalAnalysisML.Stemmer.Tests
+
+open SentimentalAnalysisML.Stemmer
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
